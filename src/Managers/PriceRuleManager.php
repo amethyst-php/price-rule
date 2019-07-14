f97cb4616a4d00569998223550fb98fa3635a77f
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/PriceRuleManager.php
+++ src/Managers/PriceRuleManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\PriceRule newEntity()
+ * @method \Amethyst\Schemas\PriceRuleSchema getSchema()
+ * @method \Amethyst\Repositories\PriceRuleRepository getRepository()
+ * @method \Amethyst\Serializers\PriceRuleSerializer getSerializer()
+ * @method \Amethyst\Validators\PriceRuleValidator getValidator()
+ * @method \Amethyst\Authorizers\PriceRuleAuthorizer getAuthorizer()
+ */
 class PriceRuleManager extends Manager
 {
     use ConfigurableManager;
