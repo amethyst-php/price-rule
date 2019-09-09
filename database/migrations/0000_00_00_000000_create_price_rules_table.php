3cc595f053fa1fb51a9228a69ed0ffa2bd83adaa
railken <railken@protonmail.ch>
2019-09-09 06:58:28 +0200

    index over unique in migration schema

--- database/migrations/0000_00_00_000000_create_price_rules_table.php
+++ database/migrations/0000_00_00_000000_create_price_rules_table.php
@@ -14,7 +14,7 @@ class CreatePriceRulesTable extends Migration
     {
         Schema::create(Config::get('amethyst.price-rule.data.price-rule.table'), function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name')->index();
             $table->text('description')->nullable();
             $table->string('class_name');
             $table->text('payload')->nullable();
